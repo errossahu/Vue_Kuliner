09972341f7a4f0a7beae8319c15c46e9433071b1
Yohanes Erros Sahu <duragonprojek@gmail.com>
2021-03-05 18:49:26 +0800

    Navbar

---
+++ src/views/Foods.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Navbar></Navbar>
+
+        <h2>Daftar Makanan</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Foods",
+    components:{
+        Navbar
+    }
+}
+</script>
+
+<style>
+
+</style>
