aff0bba23e8aa30e8712c0ef223ccf381876d288
Yohanes Erros Sahu <duragonprojek@gmail.com>
2021-03-05 20:42:55 +0800

    Desain Navbar

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="row">
+  <div class="row mt-4">
       <div class="col">
-          <h2><strong>Delicius Food Home</strong></h2>
+         <div class="d-flex 1-100">
+             <div class="justify-content-center align-self-center">
+                              <h2><strong>Food For Everyone</strong><br>Yummy...!</h2>
+                              <p>Ayo Segera Pilih Makanan Favoritmu</p>
+                              <button class="btn btn-lg btn-success"><b-icon-arrow-right></b-icon-arrow-right>Pesan</button>
+             </div>
+
+         </div>
       </div>
-      <div class="col"></div>
+      <div class="col">
+
+          <img src="../assets/Untitled.png" width="100%">
+      </div>
   </div>
 </template>
 
